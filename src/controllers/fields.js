fe3c1180038a5c22916c7b2d0e7967c64ff2a0a5
Pavel Smolin <isingwithaz@gmail.com>
2019-03-08 23:00:36 +0300

    Update fields controller

--- src/controllers/fields.js
+++ src/controllers/fields.js
@@ -3,12 +3,12 @@ import { Field } from '../models';
 const getAll = (req, res) => {
   Field.find()
     .populate('user', ['name', 'avatar'])
-    .then(profiles => {
-      if (!profiles) {
+    .then(fields => {
+      if (!fields) {
         return res.status(404).json({ nofield: 'There are no fields' });
       }
 
-      res.json(profiles);
+      res.json(fields);
     })
     .catch(() => res.status(404).json({ field: 'There are no fields' }));
 };
@@ -31,9 +31,16 @@ const getById = (req, res) => {
 const create = (req, res) => {
   const { id } = req.user;
 
-  const { name, lat, long } = req.body;
+  const { address, description, lat, long, name, phones, photos, type, url } = req.body;
 
-  const newFieldData = { user: id, name, location: { lat, long } };
+  const newFieldData = { user: id, name, location: { lat, long }, info: {} };
+
+  if (address) newFieldData.address = address;
+  if (description) newFieldData.description = description;
+  if (photos) newFieldData.info.photos = photos.replace(/\s/g, '').split(',');
+  if (phones) newFieldData.info.phones = phones.replace(/\s/g, '').split(',');
+  if (type) newFieldData.type = type;
+  if (url) newFieldData.info.url = url;
 
   // Check if field exists
   Field.findOne({ name }).then(field => {
@@ -41,8 +48,6 @@ const create = (req, res) => {
       res.status(400).json({ name: 'Field with this name already exists' });
     }
 
-    console.log(req.body);
-    console.log(newFieldData);
     // Save Field
     new Field(newFieldData)
       .save()
