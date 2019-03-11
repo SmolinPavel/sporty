3817f37aaeb6984b861f0c03eb0f89eeb61db39b
Pavel Smolin <isingwithaz@gmail.com>
2019-03-12 00:04:12 +0300

    Update dotenv

--- dist/controllers/fields.js
+++ dist/controllers/fields.js
@@ -8,14 +8,14 @@ exports.default = void 0;
 var _models = require("../models");
 
 var getAll = function getAll(req, res) {
-  _models.Field.find().populate('user', ['name', 'avatar']).then(function (profiles) {
-    if (!profiles) {
+  _models.Field.find().populate('user', ['name', 'avatar']).then(function (fields) {
+    if (!fields) {
       return res.status(404).json({
         nofield: 'There are no fields'
       });
     }
 
-    res.json(profiles);
+    res.json(fields);
   }).catch(function () {
     return res.status(404).json({
       field: 'There are no fields'
@@ -46,17 +46,30 @@ var getById = function getById(req, res) {
 var create = function create(req, res) {
   var id = req.user.id;
   var _req$body = req.body,
-      name = _req$body.name,
+      address = _req$body.address,
+      description = _req$body.description,
       lat = _req$body.lat,
-      long = _req$body.long;
+      long = _req$body.long,
+      name = _req$body.name,
+      phones = _req$body.phones,
+      photos = _req$body.photos,
+      type = _req$body.type,
+      url = _req$body.url;
   var newFieldData = {
     user: id,
     name: name,
     location: {
       lat: lat,
       long: long
-    }
-  }; // Check if field exists
+    },
+    info: {}
+  };
+  if (address) newFieldData.address = address;
+  if (description) newFieldData.description = description;
+  if (photos) newFieldData.info.photos = photos.replace(/\s/g, '').split(',');
+  if (phones) newFieldData.info.phones = phones.replace(/\s/g, '').split(',');
+  if (type) newFieldData.type = type;
+  if (url) newFieldData.info.url = url; // Check if field exists
 
   _models.Field.findOne({
     name: name
@@ -65,10 +78,8 @@ var create = function create(req, res) {
       res.status(400).json({
         name: 'Field with this name already exists'
       });
-    }
+    } // Save Field
 
-    console.log(req.body);
-    console.log(newFieldData); // Save Field
 
     new _models.Field(newFieldData).save().then(function (field) {
       return res.json(field);
@@ -78,8 +89,34 @@ var create = function create(req, res) {
   });
 };
 
+var deleteById = function deleteById(req, res) {
+  var userId = req.user.id;
+  var id = req.params.id;
+
+  _models.Field.findById(id).then(function (field) {
+    // Check for field owner
+    if (field.user.toString() !== userId) {
+      return res.status(401).json({
+        notauthorized: 'User not authorized'
+      });
+    } // Delete
+
+
+    return field.remove();
+  }).then(function () {
+    return res.json({
+      success: true
+    });
+  }).catch(function (err) {
+    return res.status(404).json({
+      postnotfound: 'No field found'
+    });
+  });
+};
+
 var _default = {
   create: create,
+  deleteById: deleteById,
   getAll: getAll,
   getById: getById
 };
--- dist/routes/1.0/fields.js
+++ dist/routes/1.0/fields.js
@@ -9,6 +9,7 @@ var _controllers = require("../../controllers");
 
 var PREFIX = '/fields';
 var create = _controllers.fields.create,
+    deleteById = _controllers.fields.deleteById,
     getAll = _controllers.fields.getAll,
     getById = _controllers.fields.getById;
 var _default = {
@@ -18,6 +19,7 @@ var _default = {
   },
   private: function _private(router) {
     router.post("".concat(PREFIX, "/create"), create);
+    router.delete("".concat(PREFIX, "/delete/:id"), deleteById);
   }
 };
 exports.default = _default;
